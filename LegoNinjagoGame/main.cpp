a4ab2a9476d0393e49d7e6934a90526626f059d0
Paulina Ostolska <pa.ostolska@gmail.com>
2022-03-07 23:32:20 +0100

    first edit

--- LegoNinjagoGame/main.cpp
+++ LegoNinjagoGame/main.cpp
@@ -6,9 +6,69 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+class Character
+{
+public:
+    string name, type;
+    unsigned int defence, attack, speed, choose;
+    
+    void printData() const
+    {
+        cout << "I'm a " << type << " and my name is " << name;
+        cout << "\n=====MY ATRIBUTES====\nDefence: " << defence;
+        cout << "\nAttack: " << attack << "\nSpeed: " << speed << endl;
+        cout<<endl;
+    }
+    void readData()
+    {
+        cout<<"Choose type: "<<endl;
+           
+            cout<<"[1] Human \n";
+            cout<<"[2] Snake \n";
+            cout<<"[3] Badass\n";
+            cout<<endl;
+            cin>>choose;
+            switch(choose)
+                {
+                    case 1: type = "Human";
+                        break;
+                    case 2: type = "Snake";
+                        break;
+                    case 3: type = "Badass";
+                        break;
+                }
+        
+        cout<<"Write a name: ";
+        cin>>name;
+        cout<<"Set my atributes!"<<endl;
+        cout<<"===VALUE RANGE 0-100==="<<endl;
+        do
+        {
+        cout<<"Defence: ";
+        cin>>defence;
+        cout<<"Attack: ";
+        cin>>attack;
+        cout<<"Speed: ";
+        cin>>speed;
+        cout<<endl;
+        }while(( defence>100) || (attack>100) || (speed>100));
+    }
+};
+int main()
+{
+    Character c1,c2,c3;
+ 
+    c1.readData();
+    c1.printData();
+    c2.readData();
+    c2.printData();
+    c3.readData();
+    c3.printData();
+    
+    cout<<endl;
+    
     return 0;
 }
