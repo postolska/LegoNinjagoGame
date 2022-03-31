3158e16d1fa6e001feab6d461ce65e1ad1347638
Paulina Ostolska <pa.ostolska@gmail.com>
2022-03-31 08:42:12 +0200

    31/03/22

--- LegoNinjagoGame/main.cpp
+++ LegoNinjagoGame/main.cpp
@@ -14,16 +14,24 @@ class Character
 private:
     string name, type;
     unsigned int defence, attack, speed, choose;
+    static int player;
 public:
+    void playerInfo()
+    {
+        player++;
+        cout<<"Player "<<player<<"."<<endl;
+    }
     void printData() const
     {
-        cout << "I'm a " << type << " and my name is " << name;
-        cout << "\n=====MY ATRIBUTES====\nDefence: " << defence;
+        cout<< "I'm a " << type << " and my name is " << name;
+        cout << "\n=====MY ATTRIBUTES====\nDefence: " << defence;
         cout << "\nAttack: " << attack << "\nSpeed: " << speed << endl;
         cout<<endl;
     }
     void readData()
     {
+        do
+        {
         cout<<"Choose type: "<<endl;
            
             cout<<"[1] Human \n";
@@ -31,6 +39,7 @@ public:
             cout<<"[3] Badass\n";
             cout<<endl;
             cin>>choose;
+       
             switch(choose)
                 {
                     case 1: type = "Human";
@@ -39,41 +48,55 @@ public:
                         break;
                     case 3: type = "Badass";
                         break;
+                    default:
+                        cout<<"Wrong number!"<<endl;
                 }
+        } while(choose>3 || choose<1);
         
         cout<<"Write a name: ";
         cin>>name;
-        cout<<"Set my atributes!"<<endl;
-        cout<<"===VALUE RANGE 0-100==="<<endl;
-        do
-        {
-        cout<<"Defence: ";
-        cin>>defence;
-        cout<<"Attack: ";
-        cin>>attack;
-        cout<<"Speed: ";
-        cin>>speed;
+        cout<<"The system will draw the attribute values"<<endl;
         cout<<endl;
-        }while(( defence>100) || (attack>100) || (speed>100));
+        defence=rand()%100+1;
+        attack=rand()%100+1;
+        speed=rand()%100+1;
+   
     }
+    friend void fightNight(Character c1, Character c2);
+
 };
+
+    void fightNight(Character c1,Character c2)
+{
+        
+    if (c1.attack > c2.defence) cout<<"Player 1. won a fight! Congratulations!\n";
+    else  cout<<"Player 2. won a fight! Congratulations!\n";
+        
+    if (c1.attack==c2.defence || c2.attack==c1.defence)
+    {
+        if(c1.speed > c2.speed) cout<<"Player 1. won a fight! Congratulations!\n";
+        else cout<<"Player 2. won a fight! Congratulations!\n";
+    }
+        
+}
+int Character::player=0;
 int main()
 {
-    Character c1,c2,c3;
+    Character c1,c2;
+   
  
+    c1.playerInfo();
     c1.readData();
-    c1.printData();
+    
+    c2.playerInfo();
     c2.readData();
+    
+    c1.printData();
     c2.printData();
-    c3.readData();
-    c3.printData();
     
-    cout<<endl;
-    
-    cout<<"Size of object 1 is: "<<sizeof(c1)<<" bytes"<<endl;
-    cout<<"Size of object 2 is: "<<sizeof(c2)<<" bytes"<<endl;
-    cout<<"Size of object 3 is: "<<sizeof(c3)<<" bytes"<<endl;
-    
+
+    fightNight(c1,c2);
+
     
     return 0;
 }
